92cdc5e591fc7bb58b791f6eea9522a3eeb43995
Paul Wellner Bou <paul@wellnerbou.de>
2017-10-16 17:12:01 +0200

    Add test

--- src/test/java/de/wellnerbou/chronic/logparser/GrokLogFormatLogLineParserTest.java
+++ src/test/java/de/wellnerbou/chronic/logparser/GrokLogFormatLogLineParserTest.java
@@ -15,24 +15,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GrokLogFormatLogLineParserTest {
 
 	private final String logline;
-	private final String ip;
+	private String pattern;
+	private final String expectedIp;
+	private String responseStatus;
+	private Long duration;
 
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-				{ "127.0.0.1 - - [02/Jan/2014:11:55:12 +0100] \"GET / HTTP/1.1\" 200 481 \"-\" \"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0\"", "127.0.0.1" }
+				{
+					"127.0.0.1 - - [02/Jan/2014:11:55:12 +0100] \"GET / HTTP/1.1\" 200 481 \"-\" \"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0\"",
+					"%{COMBINEDAPACHELOG}",
+					"127.0.0.1",
+					"200", 0L
+				}, {
+					"88.215.192.113, 192.168.190.5 - - [16/Sep/2017:02:36:57 +0200] \"GET /i.2910.de.rpc?drbm:theme_ids=38 HTTP/1.1\" 200 53055 429877 \"-\" \"-\"",
+					"%{IPORHOST:clientip}, %{IPORHOST:clientip2} %{USER:ident} %{USER:auth} \\[%{HTTPDATE:timestamp}\\] \"(?:%{WORD:verb} %{NOTSPACE:request}(?: HTTP/%{NUMBER:httpversion})?|%{DATA:rawrequest})\" %{NUMBER:response} (?:%{NUMBER:bytes}|-) (?:%{NUMBER:duration}|-)",
+					"88.215.192.113",
+					"200", 429877L
+				}
 		});
 	}
 
-	public GrokLogFormatLogLineParserTest(String logline, String ip) {
+	public GrokLogFormatLogLineParserTest(String logline, String pattern, String expectedIp, String responseStatus, Long duration) {
 		this.logline = logline;
-		this.ip = ip;
+		this.pattern = pattern;
+		this.expectedIp = expectedIp;
+		this.responseStatus = responseStatus;
+		this.duration = duration;
 	}
 
 	@Test
 	public void testParseLine() {
 		GrokLogFormatLogLineParser logLineParser = new GrokLogFormatLogLineParser();
+		logLineParser.init(pattern, new GrokResultMapper());
 		LogLineData logLineData = logLineParser.parseLine(logline);
-		Assertions.assertThat(logLineData.getClientip()).isEqualTo(ip);
+		Assertions.assertThat(logLineData.getClientip()).isEqualTo(expectedIp);
+		Assertions.assertThat(logLineData.getStatusCode()).isEqualTo(responseStatus);
+		Assertions.assertThat(logLineData.getDuration()).isEqualTo(duration);
 	}
 }
