54fdf376d5827372973c4aae5dd14c24d97e3efe
Imdat Maden <maden@tesolva.de>
2020-08-17 10:05:18 +0200

    create jsonResultDataLogger and jsonLogLine

--- src/test/java/de/wellnerbou/chronic/replay/JsonResultDataLoggerTest.java
+++ src/test/java/de/wellnerbou/chronic/replay/JsonResultDataLoggerTest.java
@@ -1,9 +1,11 @@
 package de.wellnerbou.chronic.replay;
 
 import com.ning.http.client.Response;
+import org.assertj.core.api.Assertions;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -14,15 +16,19 @@ import static org.mockito.Mockito.when;
 
 public class JsonResultDataLoggerTest {
 
-    JsonResultDataLogger jsonResultDataLogger = new JsonResultDataLogger();
+	JsonResultDataLogger jsonResultDataLogger = new JsonResultDataLogger();
 
-    @Test
-    public void testJsonLogging() throws URISyntaxException, MalformedURLException {
-        final LogLineData logLineData = new LogLineData();
-        logLineData.setDuration(20L);
+	@Test
+	public void testJsonLogging() throws URISyntaxException, MalformedURLException {
+		final LogLineData logLineData = new LogLineData();
+		logLineData.setDuration(20L);
+		logLineData.setStatusCode("500");
+		logLineData.setTime(Instant.now().toEpochMilli());
 
-        final Response response = Mockito.mock(Response.class);
-        when(response.getUri()).thenReturn(new URI("http://www.example.com"));
-        jsonResultDataLogger.logResultDataLine(logLineData, response, true, 30L, Instant.now().toEpochMilli());
-    }
+		final Response response = Mockito.mock(Response.class);
+		when(response.getUri()).thenReturn(new URI("http://www.example.com"));
+
+		jsonResultDataLogger.logResultDataLine(logLineData, response, Boolean.TRUE, 10, Instant.now().toEpochMilli());
+		Assertions.assertThat(logLineData.getDuration()).isEqualTo(20L);
+	}
 }
